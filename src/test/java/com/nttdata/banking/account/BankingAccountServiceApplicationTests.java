4f83817ab29c037deb4d2aaeab1ce7ae2d315e7c
mqcdev <michael.chavezjq@gmail.com>
2025-05-25 02:07:20 -0500

    chore(account): initialize banking account service project

---
+++ src/test/java/com/nttdata/banking/account/BankingAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.banking.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
